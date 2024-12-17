0027728b3425a9ea407787a0470c72ee86adcd76
SariSokolovski <s0527696785@gmail.com>
2024-12-17 23:44:56 +0200

    17/12-23:45

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/gitfKal/',
   plugins: [react()],
+  server: {
+    mimeTypes: {
+      '.jsx': 'application/javascript'
+    }
+  }
 })
